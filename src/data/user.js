2969488d6dce0fd9256414507888374975bc1f46
Defne Celikoyar <defne.celikoyar@gmail.com>
2021-12-18 22:43:18 -0500

    Completed assignment

--- src/data/user.js
+++ src/data/user.js
@@ -1,6 +1,8 @@
-const username = "Liza";
-const city = "New York";
+const username = "Defne";
+const city = "NYC";
 
 const image = "https://i.imgur.com/mV8PQxj.gif";
+
+export { username, city, image };
 
 // export all three variables as named exports
